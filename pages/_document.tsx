28f844821d20123c138af30e6b3ec2d28b55b964
maggie-j-liu <maggie.j.liu@gmail.com>
2022-07-02 22:43:39 -0700

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,14 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link
+          rel="apple-touch-icon"
+          href="/favicon-180x180.png"
+          sizes="180x180"
+        />
+        <meta name="theme-color" content="#1E40AF" />
       </Head>
       <body>
         <Main />
