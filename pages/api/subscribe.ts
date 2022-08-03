8ff3b8e166904abc3b925e6779fdfe1b7f11c574
maggie-j-liu <maggie.j.liu@gmail.com>
2022-08-02 20:25:53 -0700

    add authentication

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -10,6 +10,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(405).send("Method not allowed");
     return;
   }
+  if (
+    req.headers.authorization !== `Bearer ${process.env.GOOGLE_FORMS_TOKEN}`
+  ) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
   if (!req.body?.email) {
     res.status(400).send("Email is required");
     return;
@@ -26,12 +33,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     .where("email", "==", email)
     .limit(1)
     .get();
-  if (existing.empty) {
-    await db.collection("subscribers").doc().set({
-      email,
-      subscribedAt: FieldValue.serverTimestamp(),
-    });
+  if (!existing.empty) {
+    res.status(400).send("You are already subscribed");
+    return;
   }
+
+  await db.collection("subscribers").doc().set({
+    email,
+    subscribedAt: FieldValue.serverTimestamp(),
+  });
 
   if (!process.env.DOMAIN) {
     throw new Error("DOMAIN environment variable is not set");
@@ -47,8 +57,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   Thanks for registering for
   <a href="https://lelandhacks.com">Leland Hacks</a>!
   Leland Hacks will be held on Sunday September 18th, at the <a href="https://goo.gl/maps/wY6vCBqyEanwznf68">Vineland Branch Library</a>.
-</div>
-<div>
   For now, please join our Discord server (<a href="https://discord.gg/t7ZJ7SpFce">https://discord.gg/t7ZJ7SpFce</a>) to connect with other attendees and get updates on the hackathon.
   If you have any questions, feel free to email us at team@lelandhacks.com.
   We're looking forward to seeing you at Leland Hacks! 
